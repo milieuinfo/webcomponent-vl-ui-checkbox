8d3a8e8c41ec6aa4c3961883f285566aa4c4cf23
Tom Coemans <tom.coemans@gmail.com>
2019-03-22 17:34:20 +0100

    WEBC-3 vl-core 1.1.0 en refactoring

--- vl-checkbox.src.js
+++ vl-checkbox.src.js
@@ -1,152 +1,115 @@
-import './node_modules/vl-ui-core/vl-core.src.js';
+import { VlElement } from './node_modules/vl-ui-core/vl-core.src.js';
 
-(() => {
-    const template = document.createElement('template');
+/**
+ * `vl-checkbox``
+ * De checkbox laat de gebruiker toe om een of meerdere opties te selecteren uit een lijst. Gebruik de checkbox in formulieren.
+ * 
+ * @demo demo/vl-checkbox.html
+ */
+export class VlCheckbox extends VlElement(HTMLElement) {
+    static get _observedAttributes() {
+        return ['label', 'value', 'checked'];
+    }
 
-    template.innerHTML = `
-        <style>
-            @import '../style.css';
-        </style>
+    static get _observedChildClassAttributes() {
+        return ['block', 'single', 'disabled', 'error', 'switch'];
+    }
 
-        <label class="vl-checkbox" for="checkbox">
-            <input class="vl-checkbox__toggle" type="checkbox" id="checkbox" name="checkbox"/>
-            <div class="vl-checkbox__label">
-                <i class="vl-checkbox__box" aria-hidden="true"></i>
-            </div>
-        </label>
-    `;
+    constructor() {
+        super(`
+            <style>
+                @import '../style.css';
+            </style>
 
-    /**
-     * `vl-checkbox``
-     * De checkbox laat de gebruiker toe om een of meerdere opties te selecteren uit een lijst. Gebruik de checkbox in formulieren.
-     * 
-     * @demo demo/vl-checkbox.html
-     */
-    class VlCheckbox extends HTMLElement {
-        static get attributes() {
-            return ['label', 'value', 'checked'];
-        }
+            <label class="vl-checkbox" for="checkbox">
+                <input class="vl-checkbox__toggle" type="checkbox" id="checkbox" name="checkbox"/>
+                <div class="vl-checkbox__label">
+                    <i class="vl-checkbox__box" aria-hidden="true"></i>
+                </div>
+            </label>
+        `);
+    }
 
-        static get classAttributes() {
-            return ['block', 'single', 'disabled', 'error', 'switch'];
-        }
+    connectedCallback() {
+        this._inputElement.onchange = this._toggle;
+    }
 
-        static get observedAttributes() {
-            return VlCheckbox.attributes.concat(VlCheckbox.classAttributes);
-        }
+    get _classPrefix() {
+        return 'vl-checkbox--';
+    }
 
-        constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({ 'mode': 'open' });
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-            this._labelElement = this._shadowRoot.querySelector('.vl-checkbox');
-            this._inputElement = this._shadowRoot.querySelector('input');
-            this._checkboxLabelElement = this._labelElement.querySelector('.vl-checkbox__label');
-        }
+    get _inputElement() {
+        return this._element.querySelector('input');
+    }
 
-        connectedCallback() {
-            this._inputElement.onchange = this._toggle;
-        }
+    get _checkboxLabelElement() {
+        return this._element.querySelector('.vl-checkbox__label');
+    }
 
-        attributeChangedCallback(attrName, oldVal, newVal) {
-            if (oldVal != newVal) {
-                VlCheckbox.classAttributes.filter(attribute => {
-                    return attribute == attrName;
-                }).forEach(attribute => {
-                    if (this.getAttribute(attribute) != null) {
-                        this._labelElement.classList.add('vl-checkbox--' + attribute);
-                    } else {
-                        this._labelElement.classList.remove('vl-checkbox--' + attribute);
-                    }
-                });
+    toggle() {
+        this._inputElement.click();
+    }
 
-                switch (attrName) {
-                    case 'label':
-                        this._labelChangedCallback(newVal);
-                        break;
-                    case 'value':
-                        this._valueChangedCallback(newVal);
-                        break;
-                    case 'checked':
-                        this._checkedChangedCallback(newVal);
-                        break;
-                    case 'disabled':
-                        this._disabledChangedCallback(newVal);
-                        break;
-                    case 'single':
-                        this._singleChangedCallback(newVal);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
-        toggle() {
-            this._inputElement.click();
-        }
-
-        _toggle() {
-            let checked;
-            const parent = this.getRootNode().host;
-            if (parent._checked && Array.isArray(parent._checked)) {
-                const value = JSON.parse(this.value);
-                if (parent._checked.indexOf(value) > -1) {
-                    parent._checked.splice(parent._checked.indexOf(value), 1);
-                } else {
-                    parent._checked.push(value);
-                }
-                checked = parent._checked;
-                parent.setAttribute('checked', JSON.stringify(checked));
+    _toggle() {
+        let checked;
+        const parent = this.getRootNode().host;
+        if (parent._checked && Array.isArray(parent._checked)) {
+            const value = JSON.parse(this.value);
+            if (parent._checked.indexOf(value) > -1) {
+                parent._checked.splice(parent._checked.indexOf(value), 1);
             } else {
-                checked = this.checked;
+                parent._checked.push(value);
             }
-            this.dispatchEvent(new CustomEvent('input', { detail: checked, bubbles: true, composed: true }));
+            checked = parent._checked;
+            parent.setAttribute('checked', JSON.stringify(checked));
+        } else {
+            checked = this.checked;
+        }
+        this.dispatchEvent(new CustomEvent('input', { detail: checked, bubbles: true, composed: true }));
+    }
+
+    _labelChangedCallback(oldValue, newValue) {
+        this._label = newValue;
+        this._checkboxLabelElement.append(this._label);
+    }
+
+    _valueChangedCallback(oldValue, newValue) {
+        this._inputElement.value = newValue;
+    }
+
+    _checkedChangedCallback(oldValue, newValue) {
+        try {
+            this._checked = JSON.parse(newValue);
+        } catch (error) {
+            this._checked = newValue != undefined;
         }
 
-        _labelChangedCallback(value) {
-            this._label = value;
-            this._checkboxLabelElement.append(this._label);
-        }
-
-        _valueChangedCallback(value) {
-            this._inputElement.value = value;
-        }
-
-        _checkedChangedCallback(value) {
-            try {
-                this._checked = JSON.parse(value);
-            } catch (error) {
-                this._checked = value != undefined;
-            }
-
-            if (!Array.isArray(this._checked)) {
-                this._inputElement.checked = this._checked;
-            } else if (this._checked.indexOf(JSON.parse(this._inputElement.value)) > -1) {
-                this._inputElement.checked = true;
-            }
-        }
-
-        _disabledChangedCallback(value) {
-            this._inputElement.disabled = value != undefined;
-        }
-
-        _singleChangedCallback() {
-            [... this._checkboxLabelElement.childNodes].filter(this._isTextNode).forEach(this._removeNode);
-            const span = document.createElement('span');
-            span.classList.add('vl-u-visually-hidden');
-            span.textContent = this._label;
-            this._checkboxLabelElement.appendChild(span);
-        }
-
-        _isTextNode(node) {
-            return node.nodeType === 3;
-        }
-
-        _removeNode(node) {
-            node.remove();
+        if (!Array.isArray(this._checked)) {
+            this._inputElement.checked = this._checked;
+        } else if (this._checked.indexOf(JSON.parse(this._inputElement.value)) > -1) {
+            this._inputElement.checked = true;
         }
     }
 
-    customElements.define('vl-checkbox', VlCheckbox);
-})();
+    _disabledChangedCallback(oldValue, newValue) {
+        this._inputElement.disabled = newValue != undefined;
+    }
+
+    _singleChangedCallback() {
+        [... this._checkboxLabelElement.childNodes].filter(this._isTextNode).forEach(this._removeNode);
+        const span = document.createElement('span');
+        span.classList.add('vl-u-visually-hidden');
+        span.textContent = this._label;
+        this._checkboxLabelElement.appendChild(span);
+    }
+
+    _isTextNode(node) {
+        return node.nodeType === 3;
+    }
+
+    _removeNode(node) {
+        node.remove();
+    }
+}
+
+customElements.define('vl-checkbox', VlCheckbox);
