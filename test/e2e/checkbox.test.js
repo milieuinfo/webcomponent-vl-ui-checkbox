ac152559d034d5ca3854777850672a1df0fd69b7
cambiph <incubusattax@gmail.com>
2019-12-03 14:03:47 +0100

    Opzet pagina-object"

--- test/e2e/checkbox.test.js
+++ test/e2e/checkbox.test.js
@@ -10,5 +10,19 @@ describe('vl-checkbox', async () => {
         return vlCheckboxPage.load();
     });
 
+    it('ik kan een standaard checkbox aan- en uitvinken', async () => {
+        const checkbox1 = await vlCheckboxPage.getCheckbox1();
+        const checkbox2 = await vlCheckboxPage.getCheckbox2();
+
+        await assert.eventually.isFalse(vlCheckboxPage.isChecked(checkbox1));
+        await assert.eventually.isTrue(vlCheckboxPage.isChecked(checkbox2));
+        
+        await driver.executeScript('return arguments[0].click();', checkbox1);
+        await driver.executeScript('return arguments[0].click();', checkbox2);
+        
+        await assert.eventually.isTrue(vlCheckboxPage.isChecked(checkbox1));
+        await assert.eventually.isFalse(vlCheckboxPage.isChecked(checkbox2));
+    })
+
     after(() => driver && driver.quit());
 });
--- test/e2e/pages/vl-checkbox.page.js
+++ test/e2e/pages/vl-checkbox.page.js
@@ -1,9 +1,71 @@
 const VlCheckbox = require('../components/vl-checkbox');
 const { Page, Config } = require('vl-ui-core');
+const { By } = require('selenium-webdriver');
 
 class VlCheckboxPage extends Page {
     async _getCheckbox(selector) {
         return new VlCheckbox(this.driver, selector);
+    }
+
+    async _getCheckboxInput(selector) {
+        const checkbox = await this._getCheckbox(selector);
+        return checkbox.shadowRoot.findElement(By.css('#checkbox'));
+    }
+
+    async getCheckbox1() {
+        return await this._getCheckboxInput('#checkbox-1');
+    }
+
+    async getCheckbox2() {
+        return await this._getCheckboxInput('#checkbox-2');
+    }
+
+    async getCheckboxBlock() {
+        return await this._getCheckboxInput('#checkbox-block');
+    }
+
+    async getCheckboxError() {
+        return await this._getCheckboxInput('#checkbox-error');
+    }
+
+    async getCheckboxDisabled() {
+        return await this._getCheckboxInput('#checkbox-disabled')
+    }
+
+    async getCheckboxSingle() {
+        return await this._getCheckboxInput('#checkbox-single');
+    }
+
+    async getCheckboxSwitch() {
+        return await this._getCheckboxInput('#checkbox-switch');
+    }
+
+    async getCheckboxMulti1() {
+        return await this._getCheckboxInput('#checkbox-multi-1')
+    }
+
+    async getCheckboxMulti2() {
+        return await this._getCheckboxInput('#checkbox-multi-2');
+    }
+
+    async getCheckboxMulti3() {
+        return await this._getCheckboxInput('#checkbox-multi-3');
+    }
+
+    async getCheckboxMultiStandard1() {
+        return await this._getCheckboxInput('#checkbox-multi-st-1')
+    }    
+    
+    async getCheckboxMultiStandard2() {
+        return await this._getCheckboxInput('#checkbox-multi-st-2')
+    }    
+    
+    async getCheckboxMultiStandard3() {
+        return await this._getCheckboxInput('#checkbox-multi-st-3')
+    }
+
+    async isChecked(element) {
+        return this.driver.executeScript('return arguments[0].checked', element);
     }
 
     async load() {
