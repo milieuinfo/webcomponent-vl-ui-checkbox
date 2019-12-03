dac34799e56ce8e6bdda39fbd385cafd0aab8c9e
cambiph <incubusattax@gmail.com>
2019-12-03 16:15:23 +0100

    Fix gitignore

---
+++ test/e2e/components/vl-checkbox.js
@@ -0,0 +1,11 @@
+const { VlElement } = require('vl-ui-core');
+
+class VlCheckbox extends VlElement {  
+    constructor(driver, selector) {
+        super(driver, selector);
+    }
+
+    
+}
+
+module.exports = VlCheckbox;
