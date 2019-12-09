495eaaee4793bd4d5a511240e48a4346a24fe56b
cambiph <incubusattax@gmail.com>
2019-12-09 14:41:11 +0100

    Extra testen

--- test/e2e/components/vl-checkbox.js
+++ test/e2e/components/vl-checkbox.js
@@ -2,13 +2,38 @@ const { VlElement } = require('vl-ui-core');
 const { By } = require('selenium-webdriver');
 
 class VlCheckbox extends VlElement {
-    async isChecked() {
-        const input = await this._getInput();
-        return this.driver.executeScript('return arguments[0].checked', input);
-    }    
 
     async _getInput() {
         return this.shadowRoot.findElement(By.css('#checkbox'));
+    }
+
+    async _getLabel() {
+        return this.shadowRoot;
+    }
+
+    async _getClass() {
+        return (await this._getLabel()).getAttribute('class');
+    }
+
+    async _hasClass(clazzName) {
+        return (await this._getClass()).indexOf(clazzName) > 0;
+    }
+
+    async isChecked() {
+        const input = await this._getInput();
+        return this.driver.executeScript('return arguments[0].checked', input);
+    }
+
+    async isError() {
+        return this._hasClass('vl-checkbox--error');
+    }
+
+    async isSwitch() {
+        return this._hasClass('vl-checkbox--switch');
+    }
+
+    async isDisabled() {
+        return this._hasClass('vl-checkbox--disabled');
     }
 }
 
